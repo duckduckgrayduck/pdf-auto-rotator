119a2e080af8c53e2805c879d96213455bef97cb
s <s@pop-os.localdomain>
2023-11-09 14:52:46 -0600

    Added command line arguments for input and out pdf

--- rotate.py
+++ rotate.py
@@ -1,3 +1,4 @@
+import argparse
 import fitz
 import cv2
 import numpy as np
@@ -29,64 +30,71 @@ def skew_correction(image):
 
     return corrected_image, skew_angle
 
-input_pdf_path = "input.pdf"
-output_pdf_path = "output.pdf"
+def main(input_pdf, output_pdf):
+    pdf_document = fitz.open(input_pdf)
 
-pdf_document = fitz.open(input_pdf_path)
+    # Create a new PDF document for the corrected images
+    output_pdf_document = fitz.open()
 
-# Create a new PDF document for the corrected images
-output_pdf_document = fitz.open()
+    for page_num in range(pdf_document.page_count):
+        page = pdf_document[page_num]
 
-for page_num in range(pdf_document.page_count):
-    page = pdf_document[page_num]
-    
-    # Determine page orientation (portrait or landscape)
-    page_width = int(page.rect.width)
-    page_height = int(page.rect.height)
-    is_landscape = page_width > page_height
-    
-    images = page.get_images(full=True)
-    
-    for img_index, image in enumerate(images):
-        base_image = pdf_document.extract_image(image[0])
-        image_data = base_image["image"]
+        # Determine page orientation (portrait or landscape)
+        page_width = int(page.rect.width)
+        page_height = int(page.rect.height)
+        is_landscape = page_width > page_height
 
-        # Convert image data to NumPy array
-        image_np = np.frombuffer(image_data, dtype=np.uint8)
+        images = page.get_images(full=True)
 
-        # Decode image using OpenCV (assuming BGRA format)
-        image_cv = cv2.imdecode(image_np, cv2.IMREAD_UNCHANGED)
+        for img_index, image in enumerate(images):
+            base_image = pdf_document.extract_image(image[0])
+            image_data = base_image["image"]
 
-        # Correct skew in the image using the skew_correction method
-        corrected_image, skew_angle = skew_correction(image_cv)
+            # Convert image data to NumPy array
+            image_np = np.frombuffer(image_data, dtype=np.uint8)
 
-        # Calculate the correct coordinates for the image
-        x0, y0, x1, y1 = int(image[0]), int(image[1]), int(image[2]), int(image[3])
-        x0, y0, x1, y1 = max(0, x0), max(0, y0), min(page_width, x1), min(page_height, y1)
+            # Decode image using OpenCV (assuming BGRA format)
+            image_cv = cv2.imdecode(image_np, cv2.IMREAD_UNCHANGED)
 
-        # Calculate offset to center the image within the page
-        image_width = x1 - x0
-        image_height = y1 - y0
-        if is_landscape:
-            # For landscape pages, swap width and height before calculating offsets
-            page_width, page_height = page_height, page_width
-        x_offset = (page_width - image_width) // 2
-        y_offset = (page_height - image_height) // 2
-        
-        # Create a Rect object with the corrected coordinates and offset
-        existing_image_rect = fitz.Rect(x0 + x_offset, y0 + y_offset, x1 + x_offset, y1 + y_offset)
+            # Correct skew in the image using the skew_correction method
+            corrected_image, skew_angle = skew_correction(image_cv)
 
-        # Convert corrected image to image buffer
-        image_buffer = cv2.imencode('.png', corrected_image)[1].tobytes()
+            # Calculate the correct coordinates for the image
+            x0, y0, x1, y1 = int(image[0]), int(image[1]), int(image[2]), int(image[3])
+            x0, y0, x1, y1 = max(0, x0), max(0, y0), min(page_width, x1), min(page_height, y1)
 
-        # Convert image buffer to Pixmap object
-        corrected_pixmap = Pixmap(image_buffer)
+            # Calculate offset to center the image within the page
+            image_width = x1 - x0
+            image_height = y1 - y0
+            if is_landscape:
+                # For landscape pages, swap width and height before calculating offsets
+                page_width, page_height = page_height, page_width
+            x_offset = (page_width - image_width) // 2
+            y_offset = (page_height - image_height) // 2
 
-        # Insert the corrected image into the new PDF document with centered alignment in portrait or landscape orientation
-        output_pdf_document.new_page(width=page_width, height=page_height)
-        output_pdf_document[-1].insert_image(existing_image_rect, pixmap=corrected_pixmap)
+            # Create a Rect object with the corrected coordinates and offset
+            existing_image_rect = fitz.Rect(x0 + x_offset, y0 + y_offset, x1 + x_offset, y1 + y_offset)
 
-# Save the new PDF document with corrected images
-output_pdf_document.save(output_pdf_path)
-output_pdf_document.close()
-pdf_document.close()
+            # Convert corrected image to image buffer
+            image_buffer = cv2.imencode('.png', corrected_image)[1].tobytes()
+
+            # Convert image buffer to Pixmap object
+            corrected_pixmap = Pixmap(image_buffer)
+
+            # Insert the corrected image into the new PDF document with centered alignment in portrait or landscape orientation
+            output_pdf_document.new_page(width=page_width, height=page_height)
+            output_pdf_document[-1].insert_image(existing_image_rect, pixmap=corrected_pixmap)
+
+    # Save the new PDF document with corrected images
+    output_pdf_document.save(output_pdf)
+    output_pdf_document.close()
+
+    # Close the original PDF document
+    pdf_document.close()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Correct skew in images within a PDF file.")
+    parser.add_argument("input_pdf", help="Input PDF file path")
+    parser.add_argument("output_pdf", help="Output PDF file path")
+    args = parser.parse_args()
+    main(args.input_pdf, args.output_pdf)
