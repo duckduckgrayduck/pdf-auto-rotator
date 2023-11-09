7e8299725bd87f424b0b7f877e751fec44d83c1c
s <s@pop-os.localdomain>
2023-11-09 14:47:43 -0600

    Detect lines using Probabilistic Hough Line Transforms

--- rotate.py
+++ rotate.py
@@ -4,39 +4,30 @@ import numpy as np
 from fitz import Pixmap
 
 def skew_correction(image):
-    # Check the number of channels in the input image
-    if image.shape[-1] == 3:
-        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    elif image.shape[-1] == 4: 
-        gray = cv2.cvtColor(image, cv2.COLOR_BGRA2GRAY)
-    else:
-        # If the number of channels is not 3 or 4, handle it accordingly (e.g., raise an error or convert to grayscale)
-        raise ValueError("Unsupported number of channels in the input image")
-
     # Apply edge detection
-    edges = cv2.Canny(gray, 50, 150, apertureSize=3)
+    edges = cv2.Canny(image, 50, 150, apertureSize=3)
 
-    # Detect lines in the image using Hough Line Transform
-    lines = cv2.HoughLines(edges, 1, np.pi / 180, 100)
+    # Detect lines in the image using Probabilistic Hough Line Transform
+    lines = cv2.HoughLinesP(edges, 1, np.pi / 180, threshold=100, minLineLength=100, maxLineGap=10)
 
     # Calculate skew angle
     skew_angle = 0
-    if lines is not None:
+    if lines is not None and len(lines) > 0:
+        total_skew_angle = 0
+        total_lines = len(lines)
         for line in lines:
-            rho, theta = line[0]
-            if theta != 0:
-                skew_angle += theta
-        skew_angle /= len(lines)
-        skew_angle = np.degrees(skew_angle) - 90  # Convert radians to degrees and adjust for vertical text
-
-    # Rotate the image to correct skew
+            x1, y1, x2, y2 = line[0]
+            angle = np.degrees(np.arctan2(y2 - y1, x2 - x1))
+            total_skew_angle += angle
+        skew_angle = total_skew_angle / total_lines
+    
+    # Correct skew in the image
     (h, w) = image.shape[:2]
     center = (w // 2, h // 2)
     rotation_matrix = cv2.getRotationMatrix2D(center, skew_angle, 1.0)
     corrected_image = cv2.warpAffine(image, rotation_matrix, (w, h), flags=cv2.INTER_LINEAR, borderMode=cv2.BORDER_REPLICATE)
 
     return corrected_image, skew_angle
-
 
 input_pdf_path = "input.pdf"
 output_pdf_path = "output.pdf"
@@ -48,8 +39,11 @@ output_pdf_document = fitz.open()
 
 for page_num in range(pdf_document.page_count):
     page = pdf_document[page_num]
-    portrait_width = int(min(page.rect.width, page.rect.height))
-    portrait_height = int(max(page.rect.width, page.rect.height))
+    
+    # Determine page orientation (portrait or landscape)
+    page_width = int(page.rect.width)
+    page_height = int(page.rect.height)
+    is_landscape = page_width > page_height
     
     images = page.get_images(full=True)
     
@@ -60,28 +54,27 @@ for page_num in range(pdf_document.page_count):
         # Convert image data to NumPy array
         image_np = np.frombuffer(image_data, dtype=np.uint8)
 
-        # Decode image using OpenCV
+        # Decode image using OpenCV (assuming BGRA format)
         image_cv = cv2.imdecode(image_np, cv2.IMREAD_UNCHANGED)
 
-        # Correct skew in the image
+        # Correct skew in the image using the skew_correction method
         corrected_image, skew_angle = skew_correction(image_cv)
 
         # Calculate the correct coordinates for the image
         x0, y0, x1, y1 = int(image[0]), int(image[1]), int(image[2]), int(image[3])
-        x0, y0, x1, y1 = max(0, x0), max(0, y0), min(portrait_width, x1), min(portrait_height, y1)
+        x0, y0, x1, y1 = max(0, x0), max(0, y0), min(page_width, x1), min(page_height, y1)
 
-        # Calculate offset to center the image within the portrait-oriented page
+        # Calculate offset to center the image within the page
         image_width = x1 - x0
         image_height = y1 - y0
-        x_offset = (portrait_width - image_width) // 2
-        y_offset = (portrait_height - image_height) // 2
-
+        if is_landscape:
+            # For landscape pages, swap width and height before calculating offsets
+            page_width, page_height = page_height, page_width
+        x_offset = (page_width - image_width) // 2
+        y_offset = (page_height - image_height) // 2
+        
         # Create a Rect object with the corrected coordinates and offset
         existing_image_rect = fitz.Rect(x0 + x_offset, y0 + y_offset, x1 + x_offset, y1 + y_offset)
-
-        # Apply skew angle correction to the Pixmap object
-        rotation_matrix = cv2.getRotationMatrix2D((corrected_image.shape[1] // 2, corrected_image.shape[0] // 2), skew_angle, 1.0)
-        corrected_image = cv2.warpAffine(corrected_image, rotation_matrix, (corrected_image.shape[1], corrected_image.shape[0]), flags=cv2.INTER_LINEAR, borderMode=cv2.BORDER_REPLICATE)
 
         # Convert corrected image to image buffer
         image_buffer = cv2.imencode('.png', corrected_image)[1].tobytes()
@@ -89,8 +82,8 @@ for page_num in range(pdf_document.page_count):
         # Convert image buffer to Pixmap object
         corrected_pixmap = Pixmap(image_buffer)
 
-        # Insert the corrected image into the new PDF document with centered alignment in portrait orientation
-        output_pdf_document.new_page(width=portrait_width, height=portrait_height)  # Use portrait dimensions
+        # Insert the corrected image into the new PDF document with centered alignment in portrait or landscape orientation
+        output_pdf_document.new_page(width=page_width, height=page_height)
         output_pdf_document[-1].insert_image(existing_image_rect, pixmap=corrected_pixmap)
 
 # Save the new PDF document with corrected images
